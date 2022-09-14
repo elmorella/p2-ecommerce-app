a692265598533f3fc38f56992d96718fcf8cf66a
john <johhab10@gmail.com>
2022-09-13 20:56:25 -0400

    added angular, used template for product page

--- Angular-FrontEnd/src/app/app.module.ts
+++ Angular-FrontEnd/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { LoginComponent } from './login/login.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { AccountComponent } from './account/account.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    LoginComponent,
+    ProductPageComponent,
+    AccountComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
