de1e72ecf300e1e11839c50013d3544f5c9694a5
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-18 14:02:02 -0700

    test data binding HomeComponent home-page.html

--- Angular-FrontEnd/src/app/pages/home-page/home-page.component.ts
+++ Angular-FrontEnd/src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthCertificate } from 'src/app/model/authCertificate.model';
+import { User } from 'src/app/model/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,12 +9,13 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
+  user: User;
   constructor(private authService: AuthService) {
     let certificate: AuthCertificate = this.authService.getAuthCert();
     console.log('HOME PAGE');
     console.log('Authorization token: ' + certificate.authToken);
-    console.log('home page. Username: ' + certificate.user?.name + " isAuthorized: " + certificate.isAuthorized)
+    console.log('home page. Username: ' + certificate.user?.username + " isAuthorized: " + certificate.isAuthorized)
+    this.user = certificate.user!;
    }
 
   ngOnInit(): void {
