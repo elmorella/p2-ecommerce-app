154d9312b4a7afb7747346e57db7a7217d4da41d
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-18 15:04:14 -0700

    minor routing module change

--- Angular-FrontEnd/src/app/app-routing.module.ts
+++ Angular-FrontEnd/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  { path: '' , redirectTo: 'login', pathMatch: 'full'},
   { path: 'home', component: HomePageComponent},
   { path: 'login', component: LoginComponent}
 ];
