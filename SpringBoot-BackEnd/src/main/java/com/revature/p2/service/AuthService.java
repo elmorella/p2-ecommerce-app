a638d7167ae04f92e45ed6bb53fddec99571ae65
Kurt Jeffries <elrordoomhammer@gmail.com>
2022-09-19 12:40:52 -0400

    added auth certificate logic

---
+++ SpringBoot-BackEnd/src/main/java/com/revature/p2/service/AuthService.java
@@ -0,0 +1,37 @@
+package com.revature.p2.service;
+
+import com.revature.p2.exception.UserNotFoundException;
+import com.revature.p2.model.AuthCertificate;
+import com.revature.p2.model.User;
+import com.revature.p2.repo.AuthCertification;
+import com.revature.p2.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AuthService {
+    private final AuthCertification auth;
+
+    @Autowired
+    public AuthService(AuthCertification auth){this.auth = auth;}
+
+    public boolean isValid(AuthCertificate certificate){
+        AuthCertificate database = auth.findAuthCertificateByUser(certificate.getUser())
+                .orElseThrow(() -> new UserNotFoundException("User Certificate not found"));
+        return database.isValid();
+    }
+    public AuthCertificate getCertByUser(User user) {
+        return auth.findAuthCertificateByUser(user)
+                .orElse(new AuthCertificate());
+    }
+    public AuthCertificate addNewAuth(AuthCertificate authCertificate){
+        return  auth.save(authCertificate);
+    }
+    public void invalidate(AuthCertificate certificate){
+        certificate.setValid(false);
+        auth.save(certificate);
+    }
+
+}
--- SpringBoot-BackEnd/src/main/java/com/revature/p2/service/UserService.java
+++ SpringBoot-BackEnd/src/main/java/com/revature/p2/service/UserService.java
@@ -1,28 +1,44 @@
 package com.revature.p2.service;
 
 import com.revature.p2.exception.UserNotFoundException;
+import com.revature.p2.model.AuthCertificate;
 import com.revature.p2.model.User;
 import com.revature.p2.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional
 public class UserService{
 
     private final UserRepo userRepo;
+    private final AuthService auth;
 
     @Autowired
-    public UserService(UserRepo userRepo){this.userRepo = userRepo;}
+    public UserService(UserRepo userRepo, AuthService auth){
+        this.userRepo = userRepo;
+        this.auth = auth;
+    }
     public User addUser(User user){return userRepo.save(user);}
     public List<User> findAllUsers(){return userRepo.findAll();}
     public User updateUser(User user){return userRepo.save(user);}
-    public User validateUser(String username, String password){
+    public AuthCertificate validateUser(String username, String password){
         User user = findUserByUsername(username);
         if(user.getPassword().contentEquals(password)){
-            return user;
+            AuthCertificate certificate = auth.getCertByUser(user);
+
+            if(certificate.getToken() == null){
+                UUID uuid = UUID.randomUUID();
+                String token = uuid.toString();
+                certificate.setValid(true);
+                certificate.setToken(token);
+                certificate.setUser(user);
+            }
+
+            return certificate;
         } else{
             throw new UserNotFoundException("User not found");
         }
