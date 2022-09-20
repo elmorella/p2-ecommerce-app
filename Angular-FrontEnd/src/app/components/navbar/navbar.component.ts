6dc4eab58f86bbc9d7997b1a24f1e71f498a6bae
Kurt Jeffries <elrordoomhammer@gmail.com>
2022-09-20 19:21:19 -0400

    Added auth and navbar changes

--- Angular-FrontEnd/src/app/components/navbar/navbar.component.ts
+++ Angular-FrontEnd/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/services/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(protected authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  protected logout(){
+    console.log(this.authService.getAuthCert().valid);
+    this.authService.logout();
+    console.log(this.authService.getAuthCert().valid);
+  }
 }
