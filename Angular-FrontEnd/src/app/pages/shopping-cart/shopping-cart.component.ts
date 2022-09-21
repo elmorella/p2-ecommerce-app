8ba55958d7c4e24b3af6e6ff4b0e30df109bfe64
john <johhab10@gmail.com>
2022-09-21 07:21:19 -0400

    styled some elements

--- Angular-FrontEnd/src/app/pages/shopping-cart/shopping-cart.component.ts
+++ Angular-FrontEnd/src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from 'src/app/model/item.model';
+import { OrderReceipt } from 'src/app/model/order-receipt.model';
+import { OrderReceiptServiceTsService } from 'src/app/services/order-receipt.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,19 +9,25 @@ import { Item } from 'src/app/model/item.model';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
+  shoppingCart: OrderReceipt = new OrderReceipt();
+  subtotal: number = 0;
+  taxPercent: number = .0925
+  taxAmount: number = 0;
+  total: number = 0;
+  constructor(private orderService: OrderReceiptServiceTsService) {
+   }
 
-  items:  Item[] =[{
-    "id": 1,
-    "name": "1999 Holographic First Edition",
-    "description": "A PSA 10-mint condition version of the 1999 holographic First Edition Base Set Charizard is the actual Holy Grail of Charizard cards and Pokemon cards in general",
-    "stock": 532,
-    "price": 36000.0,
-    "imageUrl": "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/02/Charizard-Cards-Most-Valuable-Pokemon-1.jpg"
-}
-];
 
   ngOnInit(): void {
     console.log('SHOPPING CART')
+    this.orderService.addToShoppingCart(    {
+      "id": 6,
+      "name": "2003 Skyridge Holographic",
+      "description": "This card is unique for several reasons as the holographic card was Colorless as opposed to the typical Fire-type.",
+      "stock": 4024,
+      "price": 12000.0,
+      "imageUrl": "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/02/Charizard-Cards-Most-Valuable-Pokemon-6.jpg"
+  })
     this.shoppingCart = this.orderService.getShoppingCart()
     console.log(this.shoppingCart.user?.name + "'s shopping cart")
     console.log(this.shoppingCart.items)
@@ -27,5 +35,5 @@ export class ShoppingCartComponent implements OnInit {
     for(let item of this.shoppingCart.items){
       this.subtotal += item.price
     }
-
+  }
 }
