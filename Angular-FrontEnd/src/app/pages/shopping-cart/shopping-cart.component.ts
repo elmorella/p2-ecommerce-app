9dd21fb3ce97ce4b52e0c61fc4d64028f4f966d2
john <johhab10@gmail.com>
2022-09-20 21:23:07 -0400

    added shopping cart page

--- Angular-FrontEnd/src/app/pages/shopping-cart/shopping-cart.component.ts
+++ Angular-FrontEnd/src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/model/item.model';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
+
+  items:  Item[] =[{
+    "id": 1,
+    "name": "1999 Holographic First Edition",
+    "description": "A PSA 10-mint condition version of the 1999 holographic First Edition Base Set Charizard is the actual Holy Grail of Charizard cards and Pokemon cards in general",
+    "stock": 532,
+    "price": 36000.0,
+    "imageUrl": "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/02/Charizard-Cards-Most-Valuable-Pokemon-1.jpg"
+}
+];
 
   constructor() { }
 
