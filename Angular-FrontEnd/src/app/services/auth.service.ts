48e660a21f5b0b06e4a543e83254e57c445fd8df
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-21 07:41:56 -0700

    fix logout bug

--- Angular-FrontEnd/src/app/services/auth.service.ts
+++ Angular-FrontEnd/src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Credentials } from '../model/credentials.model';
 import { AuthCertificate } from '../model/auth-certificate.model';
+import { OrderReceiptServiceTsService} from 'src/app/services/order-receipt.service'
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class AuthService {
 
   BASE_URL = 'http://localhost:8080'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private orderService: OrderReceiptServiceTsService) { }
 
   public doLoginAttempt(credentials: Credentials) : Observable<any>{
     return this.http.post<any>(`${this.BASE_URL}/user/login`, credentials);
@@ -35,6 +36,6 @@ export class AuthService {
     this.http.put<AuthCertificate>(`${this.BASE_URL}/auth/invalidate`, this.authCert).subscribe((response: AuthCertificate) =>{
       this.authCert = response;
     });
-    
+    this.orderService.clearShoppingCart();
   }
 }
